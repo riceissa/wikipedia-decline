d3c5c1255f89046c137af546af9ec92236ad46b5
Issa Rice <riceissa@gmail.com>
2016-09-18 17:26:15 -0700

    clean up get_df

--- plot.py
+++ plot.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 
-def get_dfs(fname, win_len):
+def get_df(fname, win_len):
     '''
-    Return (raw, rolling), where raw is the raw pageviews dataframe and rolling
-    is the rolling mean pageview dataframe, normalized by the number of dats in
-    the time period.
+    Return the rolling mean pageview dataframe, normalized by the number of
+    dates in the time period. When win_len is 1, this is of course just the raw
+    monthly pageviews that was passed in.
     '''
     df = pd.read_csv(fname, sep='|')
     df = df[:-2] # remove "Total" and "Percentage" rows from the bottom
@@ -21,8 +21,6 @@ def get_dfs(fname, win_len):
     rolling = pd.rolling_sum(df, window=win_len)
     tmp = rolling.copy()
     for i in range(win_len-1, len(rolling)):
-        # TODO use the month ends to calculate the period
-        # TODO win_len=1 should return raw df
         d0 = tmp.iloc[i-win_len+1].name
         d1 = tmp.iloc[i].name
         d1_eom = pd.to_datetime(datetime.date(d1.year, d1.month,
@@ -31,38 +29,34 @@ def get_dfs(fname, win_len):
         #   [beginning of previous month] + 1, to include the final day
         num_days = (d1_eom - d0).days + 1
         rolling.iloc[i] = tmp.iloc[i].divide(num_days)
+    return rolling
 
-    rolling = rolling.divide(rolling.index.days_in_month, axis='index')
-    return (df, rolling)
+# def raw_df(fname):
+#     df = pd.read_csv(fname, sep='|')
+#     df = df[:-2] # remove "Total" and "Percentage" rows from the bottom
+#     df['Month'] = pd.to_datetime(df['Month'].map(lambda x:
+#         str(x)[:4] + "-" + str(x)[4:]))
+#     df = df.set_index('Month')
+#     df = df.sort_index()
+#     del df['Percentage']
+#     return df
 
-    return something
-
-def raw_df(fname):
-    df = pd.read_csv(fname, sep='|')
-    df = df[:-2] # remove "Total" and "Percentage" rows from the bottom
-    df['Month'] = pd.to_datetime(df['Month'].map(lambda x:
-        str(x)[:4] + "-" + str(x)[4:]))
-    df = df.set_index('Month')
-    df = df.sort_index()
-    del df['Percentage']
-    return df
-
-def get_df(fname, win_len):
-    df = pd.read_csv(fname)
-    df = df[:-2] # remove "Total" and "Percentage" rows from the bottom
-    df['Month'] = pd.to_datetime(df['Month'].map(lambda x:
-        str(x)[:4] + "-" + str(x)[4:]))
-    df = df.set_index('Month')
-    df = df.sort_index()
-    del df['Percentage']
-    df = pd.rolling_sum(df, window=win_len)
-    tmp = df.copy()
-    for i in range(win_len-1, len(df)):
-        # TODO use the month ends to calculate the period
-        num_days = (tmp.iloc[i].name - tmp.iloc[i-win_len+1].name).days
-        df.iloc[i] = tmp.iloc[i].divide(num_days)
-    # df = df.divide(df.index.days_in_month, axis='index')
-    return df
+# def get_df(fname, win_len):
+#     df = pd.read_csv(fname)
+#     df = df[:-2] # remove "Total" and "Percentage" rows from the bottom
+#     df['Month'] = pd.to_datetime(df['Month'].map(lambda x:
+#         str(x)[:4] + "-" + str(x)[4:]))
+#     df = df.set_index('Month')
+#     df = df.sort_index()
+#     del df['Percentage']
+#     df = pd.rolling_sum(df, window=win_len)
+#     tmp = df.copy()
+#     for i in range(win_len-1, len(df)):
+#         # TODO use the month ends to calculate the period
+#         num_days = (tmp.iloc[i].name - tmp.iloc[i-win_len+1].name).days
+#         df.iloc[i] = tmp.iloc[i].divide(num_days)
+#     # df = df.divide(df.index.days_in_month, axis='index')
+#     return df
 
 df = raw_df('desktop_monthly_colors.csv')
 df_mob = raw_df('mobile_monthly_colors.csv')
