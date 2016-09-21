3bb786fa72bcc2241ff9bfbf7f8086f42b1d7be8
Issa Rice <riceissa@gmail.com>
2016-09-21 16:03:16 -0700

    make function

--- ratio_convert.py
+++ ratio_convert.py
@@ -14,8 +14,25 @@ for i in range(1, 30):
 
 pd.concat([df.iloc[:, 1:].divide(df['Michael Jackson: (Worldwide)'].max()) for df in dfs], axis=1).set_index(dfs[0]['Week'])
 
-# pd.concat([df.iloc[:, 2:].divide(df['Michael Jackson: (Worldwide)'].max()) for df in dfs] + [dfs[0].iloc[:, 1:2].divide(dfs[0]['Michael Jackson: (Worldwide)'].max())], axis=1).set_index(dfs[0]['Week'])['Michael Jackson: (Worldwide)'].plot(legend=None); plt.show()
-# gt_df = np.log10(pd.concat([df.iloc[:, 2:].divide(df['Michael Jackson: (Worldwide)'].max()) for df in dfs] + [dfs[0].iloc[:, 1:2].divide(dfs[0]['Michael Jackson: (Worldwide)'].max())], axis=1).set_index(dfs[0]['Week']))
+def plot_musicians_log_minus():
+
+    # pd.concat([df.iloc[:, 2:].divide(df['Michael Jackson: (Worldwide)'].max()) for df in dfs] + [dfs[0].iloc[:, 1:2].divide(dfs[0]['Michael Jackson: (Worldwide)'].max())], axis=1).set_index(dfs[0]['Week'])['Michael Jackson: (Worldwide)'].plot(legend=None); plt.show()
+    wv_df = get_df('data/musicians_desktop.csv', 1)
+    gt_df = pd.concat(
+            # select non-MJ column and normalize by MJ max
+            [df.iloc[:, 2:].divide(df['Michael Jackson: (Worldwide)'].max())
+                for df in dfs] +
+            # include one MJ trend too
+            [dfs[0].iloc[:, 1:2].divide(dfs[0]['Michael Jackson: (Worldwide)'
+                ].max())], axis=1)
+    gt_df = gt_df.set_index(pd.to_datetime(dfs[0]['Week']))
+    gt_df_sum = gt_df.sum(axis=1)
+    combined_df = pd.DataFrame([gt_df_sum, wv_df.Total]).T
+    combined_df.columns = ['google_trends', 'wikipedia_pageviews']
+    wv_df.append(gt_df).sort_index().resample('Q-APR', loffset='-1m')
+
+# sum of musicians
+# np.log10(gt_df.sum(axis=1))
 
 # plot with
 # pd.concat([df.iloc[:, 2:].divide(df['Michael Jackson: (Worldwide)'].max()) for df in dfs], axis=1).plot(legend=None)
